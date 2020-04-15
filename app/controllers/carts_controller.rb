4e959e61ceea5a654319e473bbd535a7f22533b5
iquery-001 <kota@vaga.tk>
2020-04-15 13:35:47 -0500

    Finished views

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,57 @@
 class CartsController < ApplicationController
+    before_action :find_cart, except: [:index, :new, :create]
+
+    def index
+        @carts = Cart.all
+    end
+
+    def show
+    end
+
+    def new
+        @cart = Cart.new
+    end
+
+    def edit
+    end
+
+    def create
+        @cart = Cart.new(cart_params)
+        if @cart.save
+          flash[:success] = "Cart successfully created"
+          redirect_to @cart
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    def update
+        if @cart.update_attributes(cart_params)
+          flash[:success] = "Cart was successfully updated"
+          redirect_to @cart
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+
+    def destroy
+        if @cart.destroy
+            flash[:success] = "Cart was successfully deleted"
+            redirect_to carts_path
+        else
+            flash[:error] = "Something went wrong"
+            redirect_to carts_path
+        end
+    end
+
+    private
+        def cart_params
+            params.require(:cart).permit(:complete, :user_id, :part_ids)
+        end
+
+        def find_cart
+            @cart = Cart.find(params[:id])
+        end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-        #   flash[:success] = "User successfully created"
+          flash[:success] = "User successfully created"
           redirect_to @user
         else
           flash[:error] = "Something went wrong"
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     def update
         if @user.update_attributes(user_params)
-        #   flash[:success] = "User was successfully updated"
+          flash[:success] = "User was successfully updated"
           redirect_to @user
         else
           flash[:error] = "Something went wrong"
@@ -38,22 +38,20 @@ class UsersController < ApplicationController
 
     def destroy
         if @user.destroy
-            # flash[:success] = "User was successfully deleted"
-            redirect_to @users_path
+            flash[:success] = "User was successfully deleted"
+            redirect_to users_path
         else
             flash[:error] = "Something went wrong"
-            redirect_to @users_path
+            redirect_to users_path
         end
     end
 
     private
-
-        def find_user
-            @user = User.find(params[:id])
-        end
-
         def user_params
             params.require(:user).permit(:name, :email)
         end
 
+        def find_user
+            @user = User.find(params[:id])
+        end
 end
