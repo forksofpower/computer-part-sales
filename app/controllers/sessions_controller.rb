48a7825b0abd5432f4ab460040732c87d28c0438
Patrick Jones <patrickjones.pmj@gmail.com>
2020-04-16 02:18:16 -0500

    create session on user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
-  def new
+    def new
     @user = User.new
-  end
+    end
 
-  def create
-  end
+    def create
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            redirect_to login_path
+        end
+    end
 
-  def destroy
-  end
+    def destroy
+    end
 end
