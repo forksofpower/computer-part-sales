97e2e7939a179c368a5f71ce700c07da6d23da40
Patrick Jones <patrickjones.pmj@gmail.com>
2020-04-16 00:08:53 -0500

    listings can have many carts

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,9 @@ class Listing < ApplicationRecord
     belongs_to :user
     belongs_to :part
 
+    has_many :cart_listings
+    has_many :carts, through: :cart_listings
+
     validates :part_id, presence: true
     validates :part_id, uniqueness: { message: 'is already listed' }
 end
