d758407ddbf77984a3184ae41094aababa7ddfc0
Patrick Jones <patrickjones.pmj@gmail.com>
2020-04-16 01:54:26 -0500

    add auth helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?
+    helper_method :current_user
+    
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
 end
